45f2e1efcb82c1b978f5478629497c9195bf16b4
looser12000 <120570402+looser12000@users.noreply.github.com>
2022-12-14 20:10:24 +0100

    Update AdventCalendar.java

--- src/edu/kit/informatik/AdventCalendar.java
+++ src/edu/kit/informatik/AdventCalendar.java
@@ -1,99 +1,142 @@
 package edu.kit.informatik;
 
 /**
- * TODO Write Javadoc
- * 
- * @author
- * @version
+ * models an advent calendar
+ * @author uqgrm
+ * @version 1.0
  */
 public class AdventCalendar {
-
+    private String[] sweets = new String[24];
+    private boolean[] isOpen = new boolean[24];
+    private int day;
+    
     /**
-     * TODO Write Javadoc
-     * 
+     * constructor sets sweets and day
      * @param sweets
+     * the sweets that go into the calendar
      */
     public AdventCalendar(String[] sweets) {
-        // TODO Implement
-        throw new UnsupportedOperationException("Implementation missing");
+        this.sweets = sweets;
+        day = 0;
     }
-
+    
     /**
-     * TODO Write Javadoc
-     * 
+     * standard getter
      * @return
+     * returns day
      */
-    public int getDay() {
-        // TODO Implement
-        throw new UnsupportedOperationException("Implementation missing");
+    public int getDay( ) {
+        return day;
     }
-
+    
     /**
-     * TODO Write Javadoc
-     * 
+     * increases day by 1 if possible
      * @return
+     * returns true, if day got increased
      */
     public boolean nextDay() {
-        // TODO Implement
-        throw new UnsupportedOperationException("Implementation missing");
+        if (day < 24) {
+            day++;
+            return true;
+        }
+        else return false; 
     }
-
+    
     /**
-     * TODO Write Javadoc
-     * 
+     * increased day by days if possible
      * @param days
+     * days to increase day by
      * @return
+     * returns true, if day got increased
      */
     public boolean nextDays(int days) {
-        // TODO Implement
-        throw new UnsupportedOperationException("Implementation missing");
+        if (day <= 0) {
+            return false;
+        }
+        else if (day + days <= 24) {
+            day = day + days;
+            return true;
+        }
+        return false;
     }
-
+    
     /**
-     * TODO Write Javadoc
-     * 
+     * checks if the door at number is open
      * @param number
+     * number of the door to check
      * @return
+     * returns true, if the door is open
      */
     public boolean isDoorOpen(int number) {
-        // TODO Implement
-        throw new UnsupportedOperationException("Implementation missing");
+        if (number <= 24 && number > 0) {
+            return isOpen[number];
+        }
+        return false;
     }
-
+    
     /**
-     * TODO Write Javadoc
-     * 
+     * opens door at number if possible
      * @param number
+     * number of the door
      * @return
+     * returns the sweet behind the door if it was closed and opens it, and null otherwise
      */
     public String openDoor(int number) {
-        // TODO Implement
-        throw new UnsupportedOperationException("Implementation missing");
+        if (number > 24 || number < 1) {
+            return null;
+        }
+        else if (number > day) {
+            return null;
+        }
+        else {
+            isOpen[number] = true;
+            return sweets[number];
+        }
     }
-
+    
     /**
-     * TODO Write Javadoc
-     * 
+     * calculates the number of doors, that could be opened until day and are not open yet
      * @return
+     * returns the number of doors, that could be opened until day and are not open yet
      */
     public int numberOfUnopenedDoors() {
-        // TODO Implement
-        throw new UnsupportedOperationException("Implementation missing");
+        if (day > 0) {
+            int count = day;
+            for (int i = 1; i <= day; i++) {
+                if (isOpen[i]) {
+                    count--;
+                }
+            }
+            return count;
+        }
+        else return 0;
     }
-
+    
     /**
-     * TODO Write Javadoc
+     * resets the calendar to its state directly after initialization
      */
     public void reset() {
-        // TODO Implement
-        throw new UnsupportedOperationException("Implementation missing");
+        isOpen = new boolean[24];
+        day = 0;
     }
-
+    
     /**
-     * TODO Write Javadoc
+     * returns the calendar as String
      */
+    @Override
     public String toString() {
-        // TODO Implement
-        throw new UnsupportedOperationException("Implementation missing");
+        String toString = "";
+        for (int i = 0; i <= 23; i++) {
+            if (isOpen[i]) {
+                toString += "[ ]";
+            }
+            else {
+                toString += "[" + sweets[i] + "]";
+            }
+            if (i == 3 || i == 7 || i == 11 || i == 15 || i == 19) {
+                toString += "\n";
+            }
+        }
+        return toString;
     }
 }
